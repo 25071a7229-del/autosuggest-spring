a43be659f964c7cada353839fefc0da02cfd778a
JOGULA SRINIDHI <25071a7229@vnrvjiet.in>
2026-07-18 12:01:08 +0530

    7

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,27 @@
 // alert("hello from js")
 console.log("hello from js")
+var users=[
+{
+    "name":"john doe",
+    "gender":"male",
+    "img":"john.png"
+},
+{
+    "name" : "jane doe",
+    "gender" : "female",
+    "img" : "jane.png"
+}
+]
+ 
+var id=0;
+function toggleUser(){
+    id=(id+1)%2
+
+    var userimage = document.getElementById("user-image");
+    userimage.src= user[id].img;
+    var username=document.getElementById("user-name");
+    username.innerHTML=users[id].name;
+    var usergender = document.getElementById("user-gender");
+    usergender.innerHTML = users[id].gender;
+
+}
